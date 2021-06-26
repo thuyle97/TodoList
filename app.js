88fdd1bfa016035e56af6604c5d61de806218bae
Thuy Le <phuongthuy9796@gmail.com>
2021-06-25 21:30:37 -0700

    add .css files

--- app.js
+++ app.js
@@ -3,36 +3,34 @@ const bodyParser = require ("body-parser");
 
 const app = express();
 
+var items = [];
+
 app.set("view engine","ejs");
+
+app.use(bodyParser.urlencoded({extened: true}));
+app.use(express.static("public"));
 
 //get route
 app.get("/", function (req, res)  {
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
-  if (currentDay === 0) {
-    day = "Sunday";
-  }
-  else if (currentDay === 1) {
-    day = "Monday";
-  }
-  else if (currentDay === 1) {
-    day = "Tuesday";
-  }
-  else if (currentDay === 1) {
-    day = "Wednesday";
-  }
-  else if (currentDay === 1) {
-    day = "Thursday";
-  }
-  else if (currentDay === 1) {
-    day = "Friday";
-  }
-  else {
-    day = "Saturday";
-  }
-  res.render("list", {kindofDay: day});
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", {kindofDay: day, newListItem: items});
 })
+
+app.post("/", function(req, res) {
+  var item = req.body.newItem;
+  items.push(item);
+
+  res.redirect("/");
+})
+
 
 app.listen(3000, function()  {
   console.log("Server is running on 3000 port.");
